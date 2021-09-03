9db9f6e08f5a49519114253c4da6eedf207e81b5
bbestbt <oranichbest@gmail.com>
2021-09-03 13:53:47 +0700

    fix loginstate

--- lib/screens.dart/main/loginstate.dart
+++ lib/screens.dart/main/loginstate.dart
@@ -1,4 +1,8 @@
+import 'package:diving_trip_agency/screens.dart/main/login.dart';
+import 'package:flutter/material.dart';
+
 class _LoginPageState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   String _password;
   String _email;
+}
