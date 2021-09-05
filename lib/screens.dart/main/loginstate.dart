16579d6318814806e70d1a384143becd4c06893e
numchok2542 <49090592+numchok2542@users.noreply.github.com>
2021-09-05 11:47:50 +0700

    Delete loginstate.dart

--- lib/screens.dart/main/loginstate.dart
+++
@@ -1,8 +0,0 @@
-import 'package:diving_trip_agency/screens.dart/main/login.dart';
-import 'package:flutter/material.dart';
-
-class _LoginPageState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String _password;
-  String _email;
-}
