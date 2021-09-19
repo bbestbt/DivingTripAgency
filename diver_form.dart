c54a4849236519a2d744f6a7fdccb1c02f7161f9
numchok2542 <49090592+numchok2542@users.noreply.github.com>
2021-09-19 12:39:41 +0700

    Add files via upload

---
+++ diver_form.dart
@@ -0,0 +1,322 @@
+import 'package:diving_trip_agency/screens.dart/main/mainScreen.dart';
+import 'package:diving_trip_agency/screens.dart/signup/diver/levelDropdown.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+//add card
+class SignupDiverForm extends StatefulWidget {
+  @override
+  _SignupDiverFormState createState() => _SignupDiverFormState();
+}
+
+class _SignupDiverFormState extends State<SignupDiverForm> {
+  // final _formKey = GlobalKey<FormState>();
+  String name;
+  String lastname;
+  // String level;
+  String phoneNumber;
+  String email;
+  String password;
+  String confirmPassword;
+  final List<String> errors = [];
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerLastname = TextEditingController();
+  //final TextEditingController _controllerLevel = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerPhone = TextEditingController();
+  final TextEditingController _controllerConfirm = TextEditingController();
+  File DiverImage;
+  File DiveBack;
+
+  void addError({String error}) {
+    if (!errors.contains(error))
+      setState(() {
+        errors.add(error);
+      });
+  }
+
+  void removeError({String error}) {
+    if (errors.contains(error))
+      setState(() {
+        errors.remove(error);
+      });
+  }
+
+
+  /// Get from gallery
+  _getPicDiver() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        DiverImage = File(pickedFile.path);
+      });
+    }
+  }
+
+  /// Get from gallery
+  _getPicCard() async {
+    PickedFile pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        DiveBack = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(children: [
+          buildNameFormField(),
+          SizedBox(height: 20),
+          buildLastnameFormField(),
+          SizedBox(height: 20),
+          LevelDropdown(),
+          SizedBox(height: 20),
+          buildEmailFormField(),
+          SizedBox(height: 20),
+          buildPhoneNumberFormField(),
+          SizedBox(height: 20),
+          buildPasswordFormField(),
+          SizedBox(height: 20),
+          buildConfirmPasswordFormField(),
+          //   FormError(errors: errors),
+          SizedBox(height: 20),
+
+          Center(child:DiverImage == null ? Text('Diver image'): kIsWeb ? Image.network(DiverImage.path,fit:BoxFit.cover,) : Image.file(File(DiverImage.path),fit:BoxFit.cover,)),
+
+          FlatButton(
+            color: Color(0xfff75BDFF),
+            child: Text(
+              'load image',
+              style: TextStyle(fontSize: 15),
+            ),
+            onPressed: () {_getPicDiver();},
+          ),
+          SizedBox(height: 20),
+
+          Center(child:DiveBack == null ? Text('Card'): kIsWeb ? Image.network(DiveBack.path,fit:BoxFit.cover,) : Image.file(File(DiveBack.path),fit:BoxFit.cover,)),
+
+          FlatButton(
+            color: Color(0xfff75BDFF),
+            child: Text(
+              'load image',
+              style: TextStyle(fontSize: 15),
+            ),
+            onPressed: () {_getPicCard();},
+          ),
+          SizedBox(height: 20),
+
+
+          FlatButton(onPressed: ()=>{Navigator.push(context, MaterialPageRoute(builder: (context) => MainScreen()))}, color: Color(0xfff75BDFF),child:  Text('Confirm',style: TextStyle(fontSize: 15),),)
+        ]),
+      ),
+    );
+  }
+
+  TextFormField buildNameFormField() {
+    return TextFormField(
+      controller: _controllerName,
+      cursorColor: Color(0xFF6F35A5),
+      keyboardType: TextInputType.name,
+      onSaved: (newValue) => name = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: "Please Enter your name");
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: "Please Enter your name");
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          hintText: "Name",
+          filled: true,
+          fillColor: Color(0xFFFd0efff),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: Icon(Icons.person)),
+    );
+  }
+
+  TextFormField buildLastnameFormField() {
+    return TextFormField(
+      controller: _controllerLastname,
+      cursorColor: Color(0xFF6F35A5),
+      keyboardType: TextInputType.name,
+      onSaved: (newValue) => lastname = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: "Please Enter your lastname");
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: "Please Enter your lastname");
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+          hintText: "Lastname",
+          filled: true,
+          fillColor: Color(0xFFFd0efff),
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          suffixIcon: Icon(Icons.person)),
+    );
+  }
+
+  TextFormField buildConfirmPasswordFormField() {
+    return TextFormField(
+      controller: _controllerConfirm,
+      obscureText: true,
+      onSaved: (newValue) => confirmPassword = newValue,
+      onChanged: (value) {
+        if (password == confirmPassword) {
+          removeError(error: "Password doesn't match");
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          return "";
+        } else if (password != value) {
+          addError(error: "Password doesn't match");
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0xFFFd0efff),
+        hintText: "Confirm password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.lock),
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      controller: _controllerPassword,
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: "Please Enter your password");
+        } else if (value.length >= 6) {
+          removeError(error: "Password is too short");
+        } else if (RegExp(
+                r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+            .hasMatch(value)) {
+          removeError(error: "Please Enter Valid Password");
+        }
+        password = value;
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: "Please Enter your password");
+          return "";
+        } else if (value.length < 6) {
+          addError(error: "Password is too short");
+          return "";
+        } else if (!(RegExp(
+                r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$'))
+            .hasMatch(value)) {
+          addError(error: "Please Enter Valid Password");
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        hintText: "Password",
+        filled: true,
+        fillColor: Color(0xFFFd0efff),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.lock),
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      controller: _controllerEmail,
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: "Please Enter your email");
+        } else if (RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+            .hasMatch(value)) {
+          removeError(error: "Please Enter Valid Email");
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: "Please Enter your email");
+          return "";
+        } else if (!(RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+"))
+            .hasMatch(value)) {
+          addError(error: "Please Enter Valid Email");
+          return "";
+        }
+
+        return null;
+      },
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0xFFFd0efff),
+        hintText: "Email",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.mail),
+      ),
+    );
+  }
+
+  TextFormField buildPhoneNumberFormField() {
+    return TextFormField(
+      controller: _controllerPhone,
+      keyboardType: TextInputType.phone,
+      onSaved: (newValue) => phoneNumber = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: "Please Enter your phone number");
+        }
+        return null;
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          addError(error: "Please Enter your phone number");
+          return "";
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Color(0xFFFd0efff),
+        hintText: "Phone number",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.phone),
+      ),
+    );
+  }
+}
