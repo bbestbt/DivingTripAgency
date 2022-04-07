ddf909c95b1728d6d21b39fe41f5d1eef5a2ffde
bbestbt <oranichbest@gmail.com>
2022-04-07 15:11:07 +0700

    fix import

--- lib/screens/diveresort/resort_details_screenold1.dart
+++ lib/screens/diveresort/resort_details_screenold1.dart
@@ -1,10 +1,10 @@
 import 'package:diving_trip_agency/controllers/menuController.dart';
+import 'package:diving_trip_agency/screens/diveresort/ResortDetailWidgetbak.dart';
 import 'package:diving_trip_agency/screens/main/components/side_menu.dart';
 import 'package:diving_trip_agency/screens/main/components/header.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:diving_trip_agency/nautilus/proto/dart/model.pb.dart';
-import 'package:diving_trip_agency/screens/diveresort/ResortDetailWidget.dart';
 
 
 class DiveResortDetailScreen extends StatelessWidget {
