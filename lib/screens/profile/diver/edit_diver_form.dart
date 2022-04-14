0ff0be173bd1ff3d45389e0d87fed34efc23edf2
numchok2542 <49090592+numchok2542@users.noreply.github.com>
2022-04-14 17:09:51 +0700

    Add files via upload

--- lib/screens/profile/diver/edit_diver_form.dart
+++ lib/screens/profile/diver/edit_diver_form.dart
@@ -316,8 +316,8 @@ class _EditDiverFormState extends State<EditDiverForm> {
                       Text('Front Image'),
                       SizedBox(width: 30),
                       Container(
-                          width: MediaQuery.of(context).size.width/10,
-                          height: MediaQuery.of(context).size.width/10,
+                          width: MediaQuery.of(context).size.width/15,
+                          height: MediaQuery.of(context).size.width/15,
                           child: user_profile.diver.documents.length == 0
                               ? new Container(
                                   color: Colors.blue,
@@ -333,7 +333,7 @@ class _EditDiverFormState extends State<EditDiverForm> {
                                 ? Image.network(
                                     DiverImage.path,
                                     fit: BoxFit.cover,
-                                    width: screenwidth * 0.2,
+                                    width: MediaQuery.of(context).size.width/15,
                                   )
                                 : Image.file(
                                     io.File(DiverImage.path),
@@ -366,8 +366,8 @@ class _EditDiverFormState extends State<EditDiverForm> {
                       Text('Back image'),
                       SizedBox(width: 30),
                       Container(
-                          width: MediaQuery.of(context).size.width/10,
-                          height: MediaQuery.of(context).size.width/10,
+                          width: MediaQuery.of(context).size.width/15,
+                          height: MediaQuery.of(context).size.width/15,
                           child: user_profile.diver.documents.length == 0
                               ? new Container(
                                   color: Colors.green,
@@ -384,7 +384,7 @@ class _EditDiverFormState extends State<EditDiverForm> {
                                   ? Image.network(
                                       DiveBack.path,
                                       fit: BoxFit.cover,
-                                      width: screenwidth * 0.2,
+                                      width: MediaQuery.of(context).size.width/15,
                                     )
                                   : Image.file(
                                       io.File(DiveBack.path),
